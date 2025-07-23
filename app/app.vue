915181321216949b197303d692a39968e7a0487a
Alray <geniusalray@outlook.com>
2025-07-24 07:03:20 +0800

    fix: wrap with UApp

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 <template>
-  <NuxtLoadingIndicator />
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <UApp>
+    <NuxtLoadingIndicator />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
 </template>
