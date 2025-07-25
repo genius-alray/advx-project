07c65edfbf9ae1570f70de669ac604606ad3c5f9
surfish <shrshr2@163.com>
2025-07-25 11:56:04 +0800

    feat(login): simplify login

--- server/api/user/login.post.ts
+++ server/api/user/login.post.ts
@@ -1,7 +1,6 @@
-import { userManager } from "~~/server/service/userManager";
 import { z } from "zod";
 
-const bodySchema = z.object({
+const _bodySchema = z.object({
   id: z.string(),
   password: z.string(),
 });
@@ -10,26 +9,35 @@ const bodySchema = z.object({
  * 用户登录
  */
 export default defineEventHandler(async (event) => {
-  const body = await readValidatedBody(event, bodySchema.parse);
-  const user = await userManager.instance.getUser(body.id);
-  if (!user) {
-    throw createError({
-      statusCode: 400,
-      statusMessage: "User not found",
-    });
-  }
-  console.log(user, body);
-  if (user.password !== body.password) {
-    throw createError({
-      statusCode: 400,
-      statusMessage: "Incorrect username or password",
-    });
-  }
+  // const body = await readValidatedBody(event, bodySchema.parse);
+  // const user = await userManager.instance.getUser(body.id);
+  // if (!user) {
+  //   throw createError({
+  //     statusCode: 400,
+  //     statusMessage: "User not found",
+  //   });
+  // }
+  // console.log(user, body);
+  // if (user.password !== body.password) {
+  //   throw createError({
+  //     statusCode: 400,
+  //     statusMessage: "Incorrect username or password",
+  //   });
+  // }
+  // await setUserSession(event, {
+  //   user: {
+  //     id: user.id,
+  //     name: user.name,
+  //   },
+  // });
+
+  // demo
+  const user = {
+    id: "admin",
+    name: "admin",
+  };
   await setUserSession(event, {
-    user: {
-      id: user.id,
-      name: user.name,
-    },
+    user,
   });
   return {
     message: "Login success",
