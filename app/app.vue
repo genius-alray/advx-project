a362b5ad081bceea970091b19e691959181f513b
Alray <geniusalray@outlook.com>
2025-07-26 18:51:55 +0800

    fix: vconsole

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+onMounted(() => {
+  if (location.search.includes("debug")) {
+    const script = document.createElement("script");
+    script.src = "https://unpkg.com/vconsole@latest/dist/vconsole.min.js";
+    document.head.appendChild(script);
+    script.addEventListener("load", () => {
+      const script2 = document.createElement("script");
+      script2.innerHTML = "new VConsole({ theme: 'dark' });";
+      document.head.appendChild(script2);
+    });
+  }
+});
+</script>
 <template>
   <UApp>
-    <ClientOnly>
-      <VConsole />
-    </ClientOnly>
     <NuxtLoadingIndicator />
     <NuxtLayout>
       <NuxtPage />
