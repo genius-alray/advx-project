806a51caf63a322887cf9e95d818058e3515569d
surfish <shrshr2@163.com>
2025-07-26 09:19:16 +0800

    feat(role): return knowledge cnt in GET /api/role/all

--- server/api/role/all.get.ts
+++ server/api/role/all.get.ts
@@ -1,10 +1,23 @@
+import { knowledgeManager } from "~~/server/service/knowledgeManager";
 import { roleManager } from "~~/server/service/roleManager";
 
 /**
  * 获取用户创建的所有 role
  */
-export default defineEventHandler(async (event) => {
-  const userId = (await requireUserSession(event)).user.id;
-  const roles = await roleManager.instance.getUserRoles(userId);
-  return roles;
-});
+export default defineEventHandler(
+  async (event): Promise<(Role & { knowledgeCount: number })[]> => {
+    const userId = (await requireUserSession(event)).user.id;
+    const roles = await roleManager.instance.getUserRoles(userId);
+    const data = [];
+    for (const role of roles) {
+      const knowledgeArr = await knowledgeManager.instance.getRoleKnowledge(
+        role.id
+      );
+      data.push({
+        ...role,
+        knowledgeCount: knowledgeArr.length,
+      });
+    }
+    return data;
+  }
+);
