49cb52a2dcfc5fe9f6c3b561d001389708476916
Alray <geniusalray@outlook.com>
2025-07-26 18:48:55 +0800

    feat: vconsole

---
+++ app/components/VConsole.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const enabled = ref(false);
+onMounted(() => {
+  if (location.search.includes("debug")) {
+    enabled.value = true;
+    const script = document.createElement("script");
+    script.src = "https://unpkg.com/vconsole@latest/dist/vconsole.min.js";
+    document.head.appendChild(script);
+    script.addEventListener("load", () => {
+      const script2 = document.createElement("script");
+      script2.innerHTML = "new VConsole({ theme: 'dark' });";
+      document.head.appendChild(script2);
+    });
+  }
+});
+</script>
+<template>
+  <div />
+</template>
